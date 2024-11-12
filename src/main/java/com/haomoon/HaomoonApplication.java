4db9d3cb03f16831914682c473aa2091826cc668
junsoo <junsoo186@gmail.com>
2024-11-12 19:04:17 +0900

    Initial commit

---
+++ src/main/java/com/haomoon/HaomoonApplication.java
@@ -0,0 +1,13 @@
+package com.haomoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaomoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaomoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haomoon/HaomoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haomoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaomoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
