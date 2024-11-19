2824d17ed70a67a3c367b1e390f4106645a60120
CNOW <wjdtpdbs1887@naver.com>
2024-11-19 20:32:51 +0900

    fix marge

--- src/main/java/com/haomoon/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/haomoon/chat/repository/ChatRoomRepository.java
@@ -1,4 +1,13 @@
 package com.haomoon.chat.repository;
 
-public class ChatRoomRepository {
+
+import com.haomoon.chat.dto.ChatRoomDTO;
+import java.util.List;
+
+public interface ChatRoomRepository {
+
+
+    void saveChatRoom(ChatRoomDTO chatRoomDTO);  // SQL 쿼리는 XML에서 처리
+
+    List<ChatRoomDTO> getChatRoomsByRoundId(Long roundId);  // SQL 쿼리는 XML에서 처리
 }
